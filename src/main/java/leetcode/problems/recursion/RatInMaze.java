a5cc730519ec2000e157febecab3411de78b37ae
Moataz Lasheen <moataz.lasheen@cipalschaubroeck.be>
2023-09-30 08:33:57 +0200

    recursion and backtracking - rat in a maze

--- src/main/java/leetcode/problems/recursion/RatInMaze.java
+++ src/main/java/leetcode/problems/recursion/RatInMaze.java
@@ -19,28 +19,24 @@ public class RatInMaze {
 			return;
 		}
 		
+		if (row < 0 || row >= n || col < 0 || col >= n || m[row][col] == 0 || visited[row][col] == true)  {
+		    return;
+		}
+		
 		visited[row][col] = true;
 		
 		// try down
-		if (row+1 < n && m[row+1][col] != 0 && !visited[row+1][col]) {
-			findPath(m, n, paths, visited, path.append("D"), row+1, col);
-			path.deleteCharAt(path.length()-1);
-		}
+		findPath(m, n, paths, visited, path.append("D"), row+1, col);
+		path.deleteCharAt(path.length()-1);
 		// try up
-		if (row > 1 && m[row-1][col] != 0 && !visited[row-1][col]) {
-			findPath(m, n, paths, visited, path.append("U"), row-1, col);
-			path.deleteCharAt(path.length()-1);
-		}
+		findPath(m, n, paths, visited, path.append("U"), row-1, col);
+		path.deleteCharAt(path.length()-1);
 		// try right
-		if (col+1 < n && m[row][col+1] != 0 && !visited[row][col+1]) {
-			findPath(m, n, paths, visited, path.append("R"), row, col+1);
-			path.deleteCharAt(path.length()-1);
-		}
+		findPath(m, n, paths, visited, path.append("R"), row, col+1);
+		path.deleteCharAt(path.length()-1);
 		// try left
-		if (col-1 > 0 && m[row][col-1] != 0 && !visited[row][col-1]) {
-			findPath(m, n, paths, visited, path.append("L"), row, col-1);
-			path.deleteCharAt(path.length()-1);
-		}
+		findPath(m, n, paths, visited, path.append("L"), row, col-1);
+		path.deleteCharAt(path.length()-1);
 		
 		visited[row][col] = false;
 		
